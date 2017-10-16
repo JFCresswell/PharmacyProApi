225dc4aacd3b2056bbc921196c4fbffaa0f63d39
John C <jcresswell@hotmail.com>
2017-10-15 22:29:26 -0500

    project setup

---
+++ PharmacyProApi.Core.BusinessLogic/Pharmacy/Authentication.cs
@@ -0,0 +1,17 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyProApi.Core.BusinessLogic.Pharmacy
+{
+    // Business Rules:
+    // 1. A fully qualified user will have an entry in the machine history table set to active
+    // 2. A new user will request access, adding a new entry in the machine history table with a confirmation code set, not yet active
+    // 3. We need to throttle attempts to add a machine history item
+    public class Authentication : IAuthentication
+    {
+    }
+}
